669f762824a065d1425b2102c8f0fb0ef7aa7dd6
skinnyK <skinnykdev@gmail.com>
2025-06-22 21:26:07 +0200

    testing setup of login/register component

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,3 +1,11 @@
 export const render = (app: HTMLElement) => {
-  app.innerHTML = `<h1>HOME!</h1>`
+  app.innerHTML = `
+  <div class="bg-amber-100 p-8 grid gap-3">
+    <h1 class="text-center">HOME</h1>
+    <div>
+      <button class="py-2 px-4 rounded-md cursor-pointer bg-green-200 hover:bg-green-400 capitalize transition-colors duration-300">log in</button>
+      <button class="py-2 px-4 rounded-md cursor-pointer bg-green-200 hover:bg-green-400 capitalize transition-colors duration-300">register</button>
+    </div>
+  </div>
+  `
 }
