18e9d3949002a08313864db93ec7475f3c7704fb
skinnyK <skinnykdev@gmail.com>
2025-06-27 15:33:34 +0200

    setting up login/register div on home/landing page

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,5 +1,11 @@
 export const render = (app: HTMLElement) => {
   app.innerHTML = `
-  <custom-card></custom-card>
+  <div class='grid gap-8 p-6 rounded-md border-2 border-white bg-amber-50 text-center'>
+    <h2 class='text-2xl font-semibold'>Welcome!</h2>
+    <div class='flex gap-3'>
+      <button class='py-2 px-4 rounded-md border-2 border-green-300 bg-green-800 hover:border-green-900 transition-colors duration-300 cursor-pointer uppercase text-amber-50 hover:text-amber-300'>register</button>
+      <button class='py-2 px-4 rounded-md border-2 border-green-300 bg-green-800 hover:border-green-900 transition-colors duration-300 cursor-pointer uppercase text-amber-50 hover:text-amber-300'>login</button>
+    </div>
+  </div>
   `
 }
