07ad8848f0f3a3e2e512c3ba86bc008237fce490
skinnyK <skinnykdev@gmail.com>
2025-06-27 15:20:47 +0200

    injected custom card component into home.ts

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,11 +1,5 @@
 export const render = (app: HTMLElement) => {
   app.innerHTML = `
-  <div class="bg-amber-100 p-8 grid gap-3">
-    <h1 class="text-center">HOME</h1>
-    <div>
-      <button class="py-2 px-4 rounded-md cursor-pointer bg-green-200 hover:bg-green-400 capitalize transition-colors duration-300">log in</button>
-      <button class="py-2 px-4 rounded-md cursor-pointer bg-green-200 hover:bg-green-400 capitalize transition-colors duration-300">register</button>
-    </div>
-  </div>
+  <custom-card></custom-card>
   `
 }
