b3d5264edda2b7a39029869b68e84bc4e6519b00
skinnyK <skinnykdev@gmail.com>
2025-10-07 16:48:13 +0200

    small wrapper style adjustment on header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,14 +2,16 @@ import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <div className="md:max-w-[1140px] md:mx-auto sm:mx-[1rem] p-4 flex items-center justify-between flex-wrap">
-      <Link
-        to={"/"}
-        className="font-bold text-2xl tracking-tight hover:text-primary-200 transition-colors text-secondary-100"></Link>
-      <nav className="flex gap-4">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/dogs"}>Dogs</Link>
-      </nav>
+    <div className="bg-primary-500 text-white shadow">
+      <div className="md:max-w-[1140px] md:mx-auto sm:mx-[1rem] p-4 flex items-center justify-between flex-wrap">
+        <Link
+          to={"/"}
+          className="font-bold text-2xl tracking-tight hover:text-primary-200 transition-colors text-secondary-100"></Link>
+        <nav className="flex gap-4">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/dogs"}>Dogs</Link>
+        </nav>
+      </div>
     </div>
   );
 };
