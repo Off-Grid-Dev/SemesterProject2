f1fa156e13f58d22b1f087783de629149fc7b41d
skinnyK <skinnykdev@gmail.com>
2025-10-07 16:50:31 +0200

    small tweak in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,9 @@ const Header = () => {
       <div className="md:max-w-[1140px] md:mx-auto sm:mx-[1rem] p-4 flex items-center justify-between flex-wrap">
         <Link
           to={"/"}
-          className="font-bold text-2xl tracking-tight hover:text-primary-200 transition-colors text-secondary-100"></Link>
+          className="font-bold text-2xl tracking-tight hover:text-primary-200 transition-colors text-secondary-100">
+          Pet Adoption
+        </Link>
         <nav className="flex gap-4">
           <Link to={"/"}>Home</Link>
           <Link to={"/dogs"}>Dogs</Link>
